d4365adde05668eb141a088b66e7ec666ba88dec
owendowell <dowellowen@gmail.com>
2022-04-24 19:11:42 -0400

    Added main class

---
+++ src/java/Sudoku.java
@@ -0,0 +1,15 @@
+/**
+ * main class for Sudoku.
+ *
+ * @author Owen Dowell
+ * @version 1.0
+ */
+public class Sudoku {
+    public static void main(String[] args) {
+        this.run();
+    }
+
+    public void run() {
+        return;
+    }
+}
