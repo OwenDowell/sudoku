e20b1110f0220292a3ec5952a4588b7cde056310
owendowell <dowellowen@gmail.com>
2022-04-24 20:00:35 -0400

    Added Number class

---
+++ src/java/components/Number.java
@@ -0,0 +1,55 @@
+/**
+ * Enumerated type containing numbers for Sudoku
+ *
+ * @author Owen Dowell
+ * @version 1.0
+ */
+public class Number {
+    private int value = 0;
+    private int solution = 0;
+    private boolean isEditable = false;
+
+    /**
+     * default constructor.  When used, sets to
+     * a blank square and turns on editability.
+     *
+     * Sets the solution field to 0, solution field
+     * is the field of the number that is supposed
+     * to be entered in the solution. This constructor
+     * will likely be most useful for testing etc.
+     */
+    public Number() {
+        this.value = 0;
+        this.solution = 0;
+        this.isEditable = true;
+    }
+
+    /**
+     * Constroctor that will set the number to
+     * a certain value and then keeps editability
+     * off. In essence, these will be the pre-
+     * populated numbers when you generate the
+     * board.
+     *
+     * @param value
+     */
+    public Number(int value) {
+        //assert that the parameter value is a valid value
+        if (value > 0 && value <= 9 )
+            this.value = value;
+            this.solution = value;
+    }
+
+    /**
+     * setBlank:
+     *
+     * Changes the number to a blank number (0),
+     * and enables editability
+     */
+    public void setBlank() {
+        this.value = 0;
+        this.editability = true;
+    }
+
+
+}
